76f306d681ba1384fa277eff65e36582a8a5c372
Jorge Britez <jorgedanielbritezg@gmail.com>
2022-02-27 22:57:33 -0300

    91 controlando estado desde padre

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -29,17 +29,33 @@ export const ShoppingPage = () => {
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
         // console.log('onProductCountChange');
 
+
         setShoppingCart( oldShoppinpCart => {
 
-            if ( count === 0 ) {
-                const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
-                return rest;
+            const productInCart: ProductInCart = oldShoppinpCart[product.id] || { ...product, count: 0 };
+
+            if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
+                productInCart.count += count;
+                return {
+                    ...oldShoppinpCart,
+                    [product.id]: productInCart,
+                }
             }
 
-            return {
-                ...oldShoppinpCart,
-                [product.id]: { ...product, count },
-            }
+            // borrar el producto
+            const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
+            return rest;
+
+
+            // if ( count === 0 ) {
+            //     const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
+            //     return rest;
+            // }
+
+            // return {
+            //     ...oldShoppinpCart,
+            //     [product.id]: { ...product, count },
+            // }
 
         });
 
