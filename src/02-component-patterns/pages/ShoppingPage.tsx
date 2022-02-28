3d3e08bf3ee47af94047aa1dc43063cb9dd39080
Jorge Britez <jorgedanielbritezg@gmail.com>
2022-02-27 22:25:03 -0300

    88 mantener sincronizados los valores

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -84,6 +84,7 @@ export const ShoppingPage = () => {
                             product={ product }
                             className="bg-dark text-white"
                             style={{ width: '100px' }}
+                            value={ product.count }
                         >
                             <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
                             <ProductButtons 
