ac830cfac429f8f1360a82bca8307de2a776805e
Jorge Britez <jorgedanielbritezg@gmail.com>
2022-02-27 23:17:51 -0300

    control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,33 +8,17 @@ export const useShoppintCart = () => {
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
       // console.log('onProductCountChange');
 
-
     setShoppingCart( oldShoppinpCart => {
 
-        const productInCart: ProductInCart = oldShoppinpCart[product.id] || { ...product, count: 0 };
-
-        if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
-            productInCart.count += count;
-            return {
-                ...oldShoppinpCart,
-                [product.id]: productInCart,
-            }
+        if ( count === 0 ) {
+            const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
+            return rest;
         }
 
-        // borrar el producto
-        const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
-        return rest;
-
-
-        // if ( count === 0 ) {
-        //     const { [product.id]: toDelete, ...rest } = oldShoppinpCart;
-        //     return rest;
-        // }
-
-        // return {
-        //     ...oldShoppinpCart,
-        //     [product.id]: { ...product, count },
-        // }
+        return {
+            ...oldShoppinpCart,
+            [product.id]: { ...product, count },
+        }
 
     });
   }
